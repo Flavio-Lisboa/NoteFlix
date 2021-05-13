34c7004efe42e3cb1e3e413c0032183231f7d06b
Flavio-Lisboa <flavio.lisboa1200@gmail.com>
2021-05-12 21:35:57 -0300

    alter function record user

--- app/Models/user.php
+++ app/Models/user.php
@@ -18,8 +18,8 @@ class User {
         if (!empty($email) and !empty($password)) {
             $email = filter_var($email, FILTER_VALIDATE_EMAIL);
             $data = $db->getList($this->table, '*', ['email_user' => $email]);
-            
             $user = $data[0];
+
             if (isset($user['id_user'])) {
                 $password = md5($password);
                 if ($password == $user['password_user']) {
@@ -33,48 +33,36 @@ class User {
 
     public function record($data) {
         $db = Database::getInstance();
-        
-        if (!empty($data['email_user']) and !empty($data['name_user']) and !empty($data['password_user'])) {
-            if (filter_var($data['email_user'], FILTER_VALIDATE_EMAIL)) {
-                $emailList = $db->getList($this->table, '*', ['email_user' => $data['email_user']]);
 
-                foreach($emailList as $db) {
-                    $email = $db['email_user'];
-                }
-                if ($data['email_user'] != $email) {
-                    if (strlen($data['name_user']) < 60) {
-                        if (strlen($data['name_user']) > 3) {
-                            if (strlen($data['password_user']) > 7) {
-                                if (strlen($data['password_user']) < 16) {
-                                    $data['password_user'] = md5($data['password_user']);
-                                    $db->insert($this->table, $data);
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            } else {
-                                return false;
-                            }
-                        } else {
-                            return false;
-                        }
-                    } else {
-                        return false;
+        if (!empty($data['email_user']) and !empty($data['name_user']) and !empty($data['password_user'])) {
+            $data['email_user'] = filter_var($data['email_user'], FILTER_VALIDATE_EMAIL);
+            $emailList = $db->getList($this->table, '*', ['email_user' => $data['email_user']]);
+            $email = $emailList[0];
+
+            if ($data['email_user'] != $email['email_user']) {
+                if (strlen($data['name_user']) < 60 && strlen($data['name_user']) > 3) {
+                    if (strlen($data['password_user']) > 7 && strlen($data['password_user']) < 16) {
+                        $data['password_user'] = md5($data['password_user']);
+                        $db->insert($this->table, $data);
+                        return true;
                     }
-                } else {
-                    return false;
                 }
-            } else {
-                return false;
             }
-        } else {
-            return false;
         }
+
+        return false;
     }
 }
 
+    
+
+    
 
 
+    
+    
+    
+        
 
 
 
